4b7fdcbfd56bd736d07e13e41d92a626fe6bad8d
Owuo John <117952763+Owuo1@users.noreply.github.com>
2026-08-14 18:48:21 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -130,7 +130,7 @@ const Footer = () => {
           paddingTop: '30px', 
           borderTop: '1px solid rgba(255,255,255,0.1)',
           opacity: 0.7,
-          color: '#e67e22',
+          color: '#4A3520',
           fontSize: '0.9rem'
         }}>
           &copy; {new Date().getFullYear()} Abba Homes Properties Ltd. All rights reserved.
