a7da5da884bae7bab398d204ed33d72234bbc3d5
Owuo John <117952763+Owuo1@users.noreply.github.com>
2026-08-15 13:26:06 +0300

    Integrate AdminPanel and update routes with properties

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,37 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import AdminPanel from './components/AdminPanel'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
 import Properties from './pages/Properties'
 import Map from './pages/Map'
 import Contact from './pages/Contact'
+import { useProperties } from './hooks/useProperties'
 import './App.css'
 
 function App() {
+  const {
+    properties,
+    addProperty,
+    deleteProperty,
+    getFeatured
+  } = useProperties()
+
   return (
     <div className="app">
       <Navbar />
+      <AdminPanel 
+        properties={properties}
+        onAddProperty={addProperty}
+        onDeleteProperty={deleteProperty}
+      />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home featuredProperties={getFeatured()} />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/properties" element={<Properties />} />
+        <Route path="/properties" element={<Properties properties={properties} />} />
         <Route path="/map" element={<Map />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
