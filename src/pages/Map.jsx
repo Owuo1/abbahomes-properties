ca1e383e34dc022a1d8a704e180435fa25b6d784
Owuo John <117952763+Owuo1@users.noreply.github.com>
2026-08-18 19:23:01 +0300

    Update Map.jsx

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -176,7 +176,7 @@ const Map = () => {
             textAlign: 'center',
             marginBottom: '1rem'
           }}>
-            🚀 Investment <span style={{ color: '#e67e22' }}>Opportunities</span>
+            Investment <span style={{ color: '#e67e22' }}>Opportunities</span>
           </h2>
           <p style={{ 
             textAlign: 'center',
